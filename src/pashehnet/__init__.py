aaa42c54774ae6ec467b2d55e3b002addef23614
Robert Bates <rob@octobang.com>
2023-11-13 22:03:40 -0500

    Stubout, adding workflow action, fixtures to tests (#1)

---
+++ src/pashehnet/__init__.py
@@ -0,0 +1,1 @@
+# from .client import HSDClient  # noqa: F401
---
+++ tests/test.py
@@ -0,0 +1,40 @@
+import os
+import pytest
+
+
+@pytest.fixture()
+def mqtt_host():
+    return os.getenv('MQTT_HOST').strip()
+
+
+@pytest.fixture()
+def mqtt_port():
+    return int(os.getenv('MQTT_PORT', '1883').strip())
+
+
+@pytest.fixture()
+def mqtt_user():
+    return os.getenv('MQTT_USER').strip()
+
+
+@pytest.fixture()
+def mqtt_pwd():
+    return os.getenv('MQTT_PWD').strip()
+
+
+@pytest.fixture()
+def mqtt_use_ssl():
+    val = os.getenv('MQTT_USE_SSL', 'false').strip().lower()
+    return val == 'true' or val == '1'
+
+
+class TestSensors:
+    pass
+
+
+class TestAPI:
+    pass
+
+
+class TestClient:
+    pass
