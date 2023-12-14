42cc532e1c8a8f0aabc614c58ad930d9bdf5bb10
Arnav Jain <arnavarnavarnav2006@gmail.com>
2023-12-13 19:03:59 -0500

    Created Sawtooth wave source, squarewave source, test classes

--- src/pashehnet/sensors/sources/__init__.py
+++ src/pashehnet/sensors/sources/__init__.py
@@ -1,2 +1,4 @@
 from .base import SensorSourceBase  # noqa: F401
 from .constant import ConstantValueSource  # noqa: F401
+from .sawtoothwave import SawtoothWaveSource  # noqa: F401
+from .squarewave import SquareWaveSource  # noqa: F401
--- tests/test_sources.py
+++ tests/test_sources.py
@@ -1,5 +1,9 @@
 from pashehnet.sensors.sources import ConstantValueSource
-
+from pashehnet.sensors.sources import SquareWaveSource
+from pashehnet.sensors.sources import SawtoothWaveSource
+import numpy as np
+from scipy import signal
+from pytest import approx
 
 class TestConstantValueSource:
     """
@@ -15,3 +19,63 @@ class TestConstantValueSource:
         src = ConstantValueSource(val)
         values = [next(src) for i in range(10)]
         assert all(v == val for v in values)
+
+
+class TestSquareWaveSource:
+    """
+    Unit tests for SquareWaveSource class
+    """
+
+    def test_source(self):
+        """
+        Test that expected square wave values are returned in iterative requests
+        """
+        # Test parameters
+        frequency, sample_rate, duty_cycle = 5, 500, 0.5
+
+        # Create SquareWaveSource object
+        src = SquareWaveSource(frequency, sample_rate, duty_cycle)
+
+        # Generate values from the SquareWaveSource
+        generated_values = [next(src) for i in range(sample_rate)]
+
+        # Generate expected values
+        t = np.linspace(0, 1, sample_rate, endpoint=False)
+        expected_values = signal.square(2 * np.pi * frequency * t, duty=duty_cycle)
+
+        # Compare generated values with expected values
+        assert generated_values == approx(expected_values)
+
+
+class TestSawtoothWaveSource:
+    """
+    Unit tests for SawtoothWaveSource class
+    """
+
+    def test_source(self):
+        """
+        Test that expected square wave values are returned in iterative requests
+        """
+        # Test parameters
+        frequency, sample_rate, width = 5, 10, 0.5
+
+        # Create SawtoothWaveSource object
+        src = SawtoothWaveSource(frequency, sample_rate, width)
+
+        # Generate values from the SquareWaveSource
+        generated_values = [next(src) for i in range(sample_rate)]
+
+        # Generate expected values
+        t = np.linspace(0, 1, sample_rate, endpoint=False)
+        expected_values = signal.sawtooth(2 * np.pi * frequency * t, width=width)
+        
+
+        # Compare generated values with expected values
+        assert generated_values == approx(expected_values)
+
+        
+
+
+
+
+
