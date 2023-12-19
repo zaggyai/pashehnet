30af7f7573b58609a3b7d8d797d8faa59f7555c7
arpieb <rob@octobang.com>
2023-12-18 22:15:11 -0500

    Implemented SeriesSource + tests

--- tests/test_sources.py
+++ tests/test_sources.py
@@ -11,6 +11,7 @@ from pashehnet.sensors.sources import (
     SawtoothWaveSource,
     FileSource,
 )
+from pashehnet.sensors.sources.series import SeriesSource
 
 
 class TestConstantValueSource:
@@ -174,6 +175,18 @@ class TestFileSource:
     def test_remote(self):
         uri = 'https://gist.githubusercontent.com/arpieb/2e0d262e4099f3c28f4befdfe0958a15/raw/e35f12955d1328998a226c550cf9a51867ce5197/gistfile1.txt'  # noqa E501
         src = FileSource(uri)
+        expected = [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 0.0]
+        sample = [next(src) for _ in range(len(expected))]
+        assert expected == approx(sample)
+
+
+class TestSeriesSource:
+    """
+    Unit tests for FileSource class
+    """
+    def test_source(self):
+        series = [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]
+        src = SeriesSource(series)
         expected = [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 0.0]
         sample = [next(src) for _ in range(len(expected))]
         assert expected == approx(sample)
