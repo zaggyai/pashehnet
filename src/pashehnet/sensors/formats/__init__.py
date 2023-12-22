22c3d5b31f5f5b4d1145133feda1369401daca27
Robert Bates <rbates@zaggy.ai>
2023-12-22 12:27:26 -0500

    Implemented SimpleFormat + tests

--- src/pashehnet/sensors/formats/__init__.py
+++ src/pashehnet/sensors/formats/__init__.py
@@ -1,3 +1,4 @@
 from .base import SensorFormatBase  # noqa: F401
 from .csv import CSVFormat  # noqa: F401
 from .json import JSONFormat  # noqa: F401
+from .simple import SimpleFormat  # noqa: F401
---
+++ src/pashehnet/sensors/formats/simple.py
@@ -0,0 +1,15 @@
+from .base import SensorFormatBase
+
+
+class SimpleFormat(SensorFormatBase):
+    """
+    Provides a plain, simple formatter that just returns the stringified value
+    """
+
+    def transform(self, value):
+        """
+        Format the value using ths builtin str()
+
+        :param value: Value to format
+        """
+        return str(value)
